a088feae2be0fc73b339b2ecedf2082b407a3bd3
tnlsmssha1 <tnlsmssha1@naver.com>
2024-02-05 22:27:46 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import { useState, useEffect } from "react";
 
+function Hello(){
+  useEffect(()=>{
+    console.log("hi");
+    return ()=> console.log("bye")
+  },[])
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue]=useState(0);
-  const [keyword, setKeyword]=useState("")
-  const onClick= ()=>setValue((prev) => prev+1);
-  const onChange= (event)=>setKeyword(event.target.value);
-  useEffect(()=>{
-    console.log("i run only once")
-  }, []);
-  useEffect(()=>{
-    console.log("i run when 'keyword' changes")
-  },[keyword])
-  useEffect(()=>{
-    console.log("i run when 'counter' changes")
-  },[counter])
-  useEffect(()=>{
-    console.log("i run when keyword and counter change")
-  },[counter, keyword])
+  const [showing, setShowing]=useState(false)
+  const onClick= () => setShowing((prev)=>!prev);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click</button>
+      <button onClick={onClick}>{showing?"hide":"show"}</button>
+      {showing? <Hello />:null}
     </div>
   );
 }
